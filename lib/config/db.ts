22947301459273aa6e89a6bde9ad1362fdca28f6
Ali Rahal <alirahal_22@outlook.com>
2021-09-26 16:59:14 +0300

    add sorting

--- lib/config/db.ts
+++ lib/config/db.ts
@@ -1,4 +1,4 @@
-import { Collection, Db, MongoClient, ObjectId } from 'mongodb';
+import { Collection, Db, MongoClient, ObjectId, SortDirection } from 'mongodb';
 import { config } from 'dotenv';
 import isEmpty from 'lodash/isEmpty';
 
@@ -64,7 +64,7 @@ export const update = <T extends BaseEntity>(
 interface PaginationAndSortingQueryParams {
   page: number;
   pageSize: string;
-  sortBy: [];
+  sortBy: [string];
 }
 
 export const find = async <T>(
@@ -72,16 +72,31 @@ export const find = async <T>(
   query: PaginationAndSortingQueryParams,
 ): Promise<[T]> => {
   const _collection = collection(name);
-  let f = _collection.find({});
+  let cursor = _collection.find({});
+  if (!isEmpty(query)) {
+    if (!isEmpty(query.sortBy)) {
+      const sortOptions = {};
+      // if only one sorting field is set, convert from string to array
+      if (typeof query.sortBy === 'string') {
+        query.sortBy = [query.sortBy];
+      }
+      // map each entry in the array to mongo filters syntax
+      query.sortBy.forEach((element) => {
+        sortOptions[element.split('.')[0]] = element.split('.')[1];
+      });
 
-  if (!isEmpty(query) && !isEmpty(query.page)) {
-    const size: number = parseInt(query.pageSize ?? '10');
-    Logger.info(`Skipping ${(query.page - 1) * size}`);
-    Logger.info(`Size ${size}`);
-    f = f.skip((query.page - 1) * size).limit(size as number);
+      Logger.info(JSON.stringify(sortOptions));
+      cursor = cursor.sort(sortOptions);
+    }
+    if (!isEmpty(query.page)) {
+      const size: number = parseInt(query.pageSize ?? '10');
+      Logger.info(`Skipping ${(query.page - 1) * size}`);
+      Logger.info(`Size ${size}`);
+      cursor = cursor.skip((query.page - 1) * size).limit(size as number);
+    }
   }
 
-  const items = f.toArray() as Promise<[T]>;
+  const items = cursor.toArray() as Promise<[T]>;
   return items;
 };
 
