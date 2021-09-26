62af773ca4f19b55cdf45b643521934b32e1399f
Ali Rahal <alirahal_22@outlook.com>
2021-09-26 23:56:13 +0300

    validate employee branch and department when creating

--- lib/modules/employee/service/add.ts
+++ lib/modules/employee/service/add.ts
@@ -1,8 +1,23 @@
 import { EMPLOYEE_COLLECTION } from '&/lib/config/collections';
 import { insert } from '&config/db';
 import Employee from '../Employee';
+import { get as getDepartment } from '../../department/service/get';
+import { get as getBranch } from '../../branch/service/get';
+import { makeError } from '&utils/makeError';
 
 export const add = async (employee: Employee) => {
+  try {
+    await getDepartment(employee.departmentId);
+  } catch (error) {
+    if (error.status == 404) throw makeError('Department not found', 404);
+  }
+
+  try {
+    await getBranch(employee.branchId);
+  } catch (error) {
+    if (error.status == 404) throw makeError('Branch not found', 404);
+  }
+
   const added = await insert(EMPLOYEE_COLLECTION, employee);
   return added;
 };
