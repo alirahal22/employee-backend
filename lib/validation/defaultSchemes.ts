0dfdbc4ab2ef3e5b962593590abfa49b80558b9a
Ali Rahal <alirahal_22@outlook.com>
2021-09-26 18:44:58 +0300

    pagination and sorting validation

--- lib/validation/defaultSchemes.ts
+++ lib/validation/defaultSchemes.ts
@@ -7,3 +7,23 @@ export const defaultGetScheme = {
     })
     .unknown(false),
 };
+
+export const defaultGetAllScheme = {
+  query: Joi.object()
+    .keys({
+      page: Joi.number().min(1).optional(),
+      pageSize: Joi.number().min(1).max(50).when('page', {
+        is: Joi.exist(),
+        then: Joi.optional(),
+        otherwise: Joi.forbidden(),
+      }),
+      sortBy: [
+        Joi.array()
+
+          .items(Joi.string().regex(/^[^.]+\.(asc|desc)$/i)) //Format needs to be property_name.asc or property_name.desc, anything else is invalid
+          .optional(),
+        Joi.string().regex(/^[^.]+\.(asc|desc)$/i), // in case only one sorting param sent
+      ],
+    })
+    .unknown(false),
+};
